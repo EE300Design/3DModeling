b87aca1cfcee0666be8d39703850f091d01fa7be
Erik Huuki <edh5193@psu.edu>
2022-01-23 22:59:14 -0500

    Fixed script to make viewable stl
    This is the example given on the numpy-stl documentation.

--- STL_demos/Cube.py
+++ STL_demos/Cube.py
@@ -1,91 +1,36 @@
-from stl import mesh
-import math
 import numpy as np
+from stl import mesh
 
-# Create 3 faces of a cube
-data = np.zeros(12, dtype=mesh.Mesh.dtype)
+# Define the 8 vertices of the cube
+vertices = np.array([\
+    [-1, -1, -1],
+    [+1, -1, -1],
+    [+1, +1, -1],
+    [-1, +1, -1],
+    [-1, -1, +1],
+    [+1, -1, +1],
+    [+1, +1, +1],
+    [-1, +1, +1]])
+# Define the 12 triangles composing the cube
+faces = np.array([\
+    [0,3,1],
+    [1,3,2],
+    [0,4,7],
+    [0,7,3],
+    [4,5,6],
+    [4,6,7],
+    [5,1,2],
+    [5,2,6],
+    [2,3,6],
+    [3,7,6],
+    [0,1,5],
+    [0,5,4]])
 
-# Top of the cube
-data['vectors'][0] = np.array([[0, 1, 1],
-                                  [1, 0, 1],
-                                  [0, 0, 1]])
-data['vectors'][1] = np.array([[1, 0, 1],
-                                  [0, 1, 1],
-                                  [1, 1, 1]])
-# Front back faces
-data['vectors'][2] = np.array([[1, 0, 0],
-                                  [1, 0, 1],
-                                  [1, 1, 0]])
-data['vectors'][3] = np.array([[1, 1, 1],
-                                  [1, 0, 1],
-                                  [1, 1, 0]])
+# Create the mesh
+cube = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+for i, f in enumerate(faces):
+    for j in range(3):
+        cube.vectors[i][j] = vertices[f[j],:]
 
-data['vectors'][4] = np.array([[0, 0, 0],
-                                  [0, 0, 1],
-                                  [0, 1, 0]])
-data['vectors'][5] = np.array([[0, 1, 1],
-                                  [0, 0, 1],
-                                  [0, 1, 0]])
-
-
-# Left face
-data['vectors'][6] = np.array([[0, 0, 0],
-                                  [1, 0, 0],
-                                  [1, 0, 1]])
-
-data['vectors'][7] = np.array([[1, 0, 1],
-                                  [0, 0, 0],
-                                  [0, 0, 1]])
-
-
-# Bottom
-data['vectors'][8] = np.array([[0, 1, 0],
-                                  [1, 0, 0],
-                                  [0, 0, 0]])
-data['vectors'][8] = np.array([[1, 0, 0],
-                                  [0, 1, 0],
-                                  [1, 1, 0]])
-# Right
-data['vectors'][10] = np.array([[0, 1, 0],
-                                  [1, 1, 0],
-                                  [1, 1, 1]])
-
-data['vectors'][11] = np.array([[0, 1, 0],
-                                  [0, 1, 1],
-                                  [1, 1, 1]])
-
-
-
-meshes = [mesh.Mesh(data.copy())]
-
-# Optionally render the rotated cube faces
-from matplotlib import pyplot
-from mpl_toolkits import mplot3d
-
-# Create a new plot
-figure = pyplot.figure()
-axes = mplot3d.Axes3D(figure)
-
-# Render the cube faces
-for m in meshes:
-    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(m.vectors))
-
-# Auto scale to the mesh size
-scale = np.concatenate([m.points for m in meshes]).flatten()
-axes.auto_scale_xyz(scale, scale, scale)
-axes.set_xlabel('X')
-axes.set_ylabel('Y')
-axes.set_zlabel('Z')
-
-# Saving the Mesh
-cube_mesh = mesh.Mesh(np.zeros(data.shape, dtype=mesh.Mesh.dtype))
-
-cube_mesh.save('cube_demo.stl')
-
-# Show the plot to the screen
-pyplot.show()
-
-
-
-
-
+# Write the mesh to file "cube.stl"
+cube.save('cube_demo.stl')
