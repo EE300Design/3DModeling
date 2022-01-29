7cbf251ab9566bd2b9bbde6b0d777d60b1f73e85
Erik Huuki <edh5193@psu.edu>
2022-01-28 20:40:36 -0500

    Cleaned up script for generating cubes and cylinders
    Need to add comments, as well as filling the data/cube and data/cylinder with files of either csv's or stl files.

--- data/cylinder_generation.py
+++ data/cylinder_generation.py
@@ -2,42 +2,92 @@ import numpy as np
 from stl import mesh
 from mpl_toolkits import mplot3d
 from matplotlib import pyplot
-import math
+
+
+
+pi = np.pi
+
 
 # Note create_cylinder generates an nxm matrix where the row index is 
 # porportional to the height and the column index is porportional
 # to the angle
 
-def create_cylinder(n,m,radius=1,rot_ax=[0,0]):
+def create_cylinder(m,n,radius=1,rot_ax=[0,0]):
 	cylndr = np.zeros([n,m])
 	for i in range(n):
-		for j in range(m):
-			x = radius*math.cos(2*math.pi/n*j)
-			y = radius*math.sin(2*math.pi/m*j)
+		theta = 2*pi/n*i
+		x = radius*np.cos(theta)
+		y = radius*np.sin(theta)
 
-			r = ((rot_ax[0]-x)**2+(rot_ax[1]-y)**2)**(1/2)
-			cylndr[i,j] = r
-
+		cylndr[:,i] = ((x-rot_ax[0])**2+(y-rot_ax[1])**2)**(1/2)
 	return cylndr
 
 
-def polar_to_cartesian(M,h=1):
 
+# column indexes at are assigned the faces of a cube
+# within ranges of angles
+def create_cube(m,n,length = 1, rot_ax = [0,0]):
+
+	cube = np.zeros([n,m])
+
+	for i in range(m):
+		theta = 2*pi/n*i
+
+		if(theta<pi/4 | theta>pi*7/4):
+			x = length
+			y = length*np.tan(theta)
+
+		elif(theta<3*pi/4):
+			y = length
+			x = -length*np.tan(theta-pi/2)
+
+		elif theta<5*pi/4:
+			x = -length
+			y = -length*np.tan(theta-pi)
+
+		elif theta<7*pi/4:
+			x = length*np.tan(theta-3*pi/2)
+			y = -length
+
+		cube[:,i] = ((x-rot_ax[0])**2+(y-rot_ax[1])**2)**(1/2)
+	return cube
+
+
+# converts these particular matrixes
+# to the dot nets in 3D space
+
+def polar_to_cart(M,h=1):
 	n,m = M.shape
 	cart = mp.zeros([n,m,3])
 
 	for i in range(n):
 		for j in range(m):
-			x = M[i,j]*math.cos(2*math.pi/m*j)
-			y = M[i,j]*math.sin(2*math.pi/m*j)
-
-			z = 5/height*i
+			x = M[i,j]*np.cos(2*pi/m*j)
+			y = M[i,j]*np.sin(2*pi/m*j)
+			z = i/n*height
+			
 			cart[i,j] = [x,y,z]
 
 	return cart
 
-# def save_stl():
+
+
+def dot_mat_2mesh(vertices,name='test.stl',save=False):
+
+	cylinder = mesh.Mesh(np.zeros(2*column_count*row_count, dtype=mesh.Mesh.dtype))
+	count = 0
+
+	for i in range(row_count-1):
+		for j in range(column_count):
+			cylinder.vectors[count] = [vertices[i,j-1],vertices[i,j],vertices[i+1,j-1]]
+			cylinder.vectors[count+1] = [vertices[i+1,j-1],vertices[i+1,j],vertices[i,j]]
+			count+=2
+	
+	if save:
+		cylinder.save(name)
+	return cylinder
 
 
 
-# def view_stl():
+
+
